90221b582f31d50b9ec2c6c3c44068814216574c
Dominic Schüchter <dschuechter@protonmail.com>
2023-04-27 18:23:53 +0200

    Updated ADC Calibration Script

--- host/LF_SFF_MIO_Calibrate_ADC.py
+++ host/LF_SFF_MIO_Calibrate_ADC.py
@@ -44,8 +44,8 @@ def calibrate_ADC():
     input_data = [[[] for i in voltages] for j in ch_list]
     input_data_err = [[[] for i in voltages] for j in ch_list]
     for adc_ch in ch_list:    
-        pltfit.beauty_plot()
-        input('Plug in %s and enter something to continue'%(adc_ch))
+        pltfit.beauty_plot(tight=False)
+        input('Plug in %s and continue by pressing [ENTER]'%(adc_ch))
         for V in voltages:
             dut['opAMP_offset'].set_voltage(offset+V)
             input_data[ch_list.index(adc_ch)][voltages.index(V)] = dut['opAMP_offset'].get_voltage()
@@ -54,13 +54,15 @@ def calibrate_ADC():
             meas = dut.read_raw_adc(nSamples, adc_ch)
             data[ch_list.index(adc_ch)][voltages.index(V)] = np.average(meas)
             data_err[ch_list.index(adc_ch)][voltages.index(V)] = np.std(meas)
-            plt.plot(meas,np.linspace(offset+np.min(voltages), np.max(voltages)+offset,len(meas)), alpha=0.3, label=str(V)+'V')
+            plt.plot(meas,np.linspace(offset+np.min(voltages), np.max(voltages)+offset,len(meas)), alpha=0.3, label=str(np.round(input_data[ch_list.index(adc_ch)][voltages.index(V)],3))+'V')
         plt.errorbar(y=input_data[ch_list.index(adc_ch)],yerr=input_data_err[ch_list.index(adc_ch)],x=data[ch_list.index(adc_ch)], xerr=data_err[ch_list.index(adc_ch)], color='black', marker='x', linestyle='None')
         popt, perr = pltfit.double_err(function=pltfit.func_lin,y=input_data[ch_list.index(adc_ch)],y_error=input_data_err[ch_list.index(adc_ch)], x=data[ch_list.index(adc_ch)], x_error=data_err[ch_list.index(adc_ch)], presets=[0,8000])        
         x = np.array([np.min(data[ch_list.index(adc_ch)])-100,np.max(data[ch_list.index(adc_ch)])+100])
         y = popt[0]*x+popt[1]
         plt.plot(x,y, label='$U(x)=(%.3f\pm%.3f)10^{-5}$V$\cdot x+(%.3f\pm%.3f)$V'%(popt[0]*1e5, perr[0]*1e5, popt[1], perr[1]), color='black')
         plt.legend()
+        plt.ylabel('ADC In Voltage / V')
+        plt.title(adc_ch+'(with applied 0.5V neg ADC input)')
         plt.savefig('./output/ADC_Calibration/'+adc_ch+'.pdf')
         dh.save_data(data=[popt, perr], output_path='./output/ADC_Calibration/data/'+adc_ch+'.csv')
         plt.show()
